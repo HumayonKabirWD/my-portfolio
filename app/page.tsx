033f92b716666fa247d24834e116c8dd2f11a14d
kabir <kaabir24124@gmail.com>
2024-11-20 07:40:09 +0600

    add suspense fallback

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Myobject from "@/components/Myobject";
 import Skill from "@/components/Skill";
 import { LoadingProvider } from "./providers/LoadingProvider";
+import { Suspense } from "react";
 
 
 
@@ -11,12 +12,16 @@ export default function Home () {
 
   return (
     <div>
+      <Suspense fallback= {<div>loading...</div>}>
+
       <LoadingProvider>
         <Hero/>
         <Myobject/>
         <Skill/>
         <About/>
       </LoadingProvider>
+
+      </Suspense>
     </div>
   );
 };
